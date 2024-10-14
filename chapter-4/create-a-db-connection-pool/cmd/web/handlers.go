fcd107b121d86f95aea7bdd0348aa6fd8f1194a2
ahnafms <ahnafmusyaffa47@gmail.com>
2024-10-14 11:54:54 +0700

    feat: chapter 4 create db connection pool

---
+++ chapter-4/create-a-db-connection-pool/cmd/web/handlers.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.hai",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		http.Error(w, "Internal server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
+}
+
+func (app *application) downloadFile(w http.ResponseWriter, r *http.Request) {
+	saveUrl := filepath.Clean(r.URL.Path)
+
+	log.Print(saveUrl)
+	r.URL.Path = saveUrl
+
+	http.ServeFile(w, r, "./ui/static/img/logo.png")
+}
+
+func (app *application) downloadFileFromUserInput(w http.ResponseWriter, r *http.Request) {
+	path := r.PathValue("path")
+	sanitizedPath := filepath.Clean(path)
+
+	basePath := "./ui/static/img/"
+
+	fullPath := basePath + sanitizedPath
+
+	log.Print(filepath.Clean(fullPath))
+
+	log.Print(fullPath)
+	http.ServeFile(w, r, fullPath)
+}
---
+++ chapter-4/create-a-db-connection-pool/cmd/web/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	log *slog.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Mysql data source name")
+
+	flag.Parse()
+
+	loggerHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true,
+	})
+	// loggerHandler := slog.NewJSONHandler(os.Stdout, nil)
+
+	logger := slog.New(loggerHandler)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
+	defer db.Close()
+
+	app := &application{
+		log: logger,
+	}
+
+	logger.Info("starting server", "addr", *addr)
+
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, err
+}
