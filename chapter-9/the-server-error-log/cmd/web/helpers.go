f0700de1d274e49781153010f2a3558b35e8adab
ahnafms <ahnafmusyaffa47@gmail.com>
2024-10-21 20:57:30 +0700

    feat: chapter 9 server error log render

--- chapter-9/the-server-error-log/cmd/web/helpers.go
+++ chapter-9/the-server-error-log/cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"net/http"
@@ -28,18 +29,23 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
 	ts, ok := app.templateCache[page]
-
 	if !ok {
-		err := fmt.Errorf("the template %s is not found", page)
+		err := fmt.Errorf("the template %s does not exist", page)
 		app.serverError(w, r, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
 	}
 
 	w.WriteHeader(status)
 
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
+	buf.WriteTo(w)
 }
 
 func (app *application) newTemplateData(r *http.Request) templateData {
